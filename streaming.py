d430190bbb30436d2ab27e569f5766f904022cee
Sean W <sean.wisnewski@gmail.com>
2021-09-22 12:39:41 -0400

    new pipeline

--- streaming.py
+++ streaming.py
@@ -6,7 +6,11 @@ import cv2
 
 
 def capture_frames():
-    cap = cv2.VideoCapture(0, cv2.CAP_V4L2)
+
+
+    # god this is ugly
+    gst_pipeline = "nvarguscamerasrc sensor-id=0 ! video/x-raw(memory:NVMM), width=(int)1920, height=(int)1080, format=(string)NV12, framerate=(fraction)30/1 ! nvvidconv ! video/x-raw ! jpegenc ! image/jpeg ! appsink"
+    cap = cv2.VideoCapture(gst_pipeline, cv2.CAP_GSTREAMER)
 
     while True:
         return_key, frame = cap.read()
