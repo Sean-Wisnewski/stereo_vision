e35cac6c2a30648972be21d33642730444479167
Sean W <sean.wisnewski@gmail.com>
2021-09-22 12:08:08 -0400

    very basic streaming of a single camera

---
+++ streaming.py
@@ -0,0 +1,25 @@
+"""
+Code pertinent to streaming from two cameras simultaneously
+"""
+
+import cv2
+
+
+def capture_frames():
+    cap = cv2.VideoCapture(0)
+
+    while cap.isOpened():
+        return_key, frame = cap.read()
+        if not return_key:
+            break
+        cv2.imshow('Cam 0', frame)
+        if cv2.waitKey(1) == 27:
+            break
+    cv2.destroyAllWindows()
+
+
+def main():
+    capture_frames()
+
+if __name__=="__main__":
+    main()
