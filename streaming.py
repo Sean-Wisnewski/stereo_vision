ace8e26aa33d45ddfc28a28b4ac511fbedb7e21d
Sean W <sean.wisnewski@gmail.com>
2021-09-22 13:01:29 -0400

    make a class

--- streaming.py
+++ streaming.py
@@ -4,17 +4,23 @@ Code pertinent to streaming from two cameras simultaneously
 
 import cv2
 
+class CameraCapture:
+    def __init__(self, sensor_id):
+        GSTREAMER_PIPELINE = f'nvarguscamerasrc sensor-id={sensor_id} ! video/x-raw(memory:NVMM), width=1920, height=1080, format=(string)NV12, framerate=30/1 ' \
+                             '! nvvidconv flip-method=0 ! video/x-raw, width=960, height=616, format=(string)BGRx ! ' \
+                             'videoconvert ! video/x-raw, format=(string)BGR ! appsink wait-on-eos=false max-buffers=1 drop=True'
+        self.cap = cv2.VideoCapture(GSTREAMER_PIPELINE, cv2.CAP_GSTREAMER)
 
-def capture_frames():
+
+def capture_frames(sensor_id=0):
 
 
     # The magic pipeline to get things to work, I have literally no idea how this works but it captures video from the csi cameras so woo
     #GSTREAMER_PIPELINE = 'nvarguscamerasrc sensor-id=1 ! video/x-raw(memory:NVMM), width=3280, height=2464, format=(string)NV12, framerate=21/1 ! nvvidconv flip-method=0 ! video/x-raw, width=960, height=616, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink wait-on-eos=false max-buffers=1 drop=True'
-    GSTREAMER_PIPELINE = 'nvarguscamerasrc sensor-id=1 ! video/x-raw(memory:NVMM), width=1920, height=1080, format=(string)NV12, framerate=30/1 ! nvvidconv flip-method=0 ! video/x-raw, width=960, height=616, format=(string)BGRx ! videoconvert ! video/x-raw, format=(string)BGR ! appsink wait-on-eos=false max-buffers=1 drop=True'
-    cap = cv2.VideoCapture(GSTREAMER_PIPELINE, cv2.CAP_GSTREAMER)
 
+    camera = CameraCapture(sensor_id)
     while True:
-        return_key, frame = cap.read()
+        return_key, frame = camera.cap.read()
         if not return_key:
             break
         cv2.imshow('Cam 0', frame)
