c9963616d428d4ab48bb4d5cfca941b21827a889
Sean W <sean.wisnewski@gmail.com>
2021-11-05 17:39:58 -0400

    updated stats to work with the new serialized format

--- concept_scripts/show_recorded_stats.py
+++ concept_scripts/show_recorded_stats.py
@@ -1,27 +1,47 @@
 import pandas as pd
 import numpy as np
+import cv2
 
 def convert_confidences(confidences):
-    pass
+    """
+    Converts a list of tensors to a single np array
+    :param confidences:
+    :return:
+    """
+    lst = []
+    for tensor in confidences:
+        for val in tensor:
+            lst.append(val.numpy())
+    return np.array(lst)
 
 def summarize_stats(df):
     # these never change so I'm hardcoding them
-    # I'm too stubborn to admit I made a mistake while recording data so I'm just rolling with it
+    # the 1: is to index only the values, and skip the row name
     fps = df.iloc[0].values[1:].astype(np.float32)
     inf_times = df.iloc[1].values[1:].astype(np.float32)
-    # these are supposed to be tensors goddamnit
     confidences = df.iloc[2].values[1:]
-    print(fps[:5])
-    print(fps.mean())
-    print(inf_times[:5])
-    print(inf_times.mean())
-    print(confidences[:5])
+    confidences = convert_confidences(confidences)
+    avg_fps = fps.mean()
+    avg_inf_time = inf_times.mean()
+    avg_conf = confidences.mean()
+    data = np.array([[avg_fps], [avg_inf_time], [avg_conf]]).T
+    cols = ['Average FPS', 'Average Inference Time', 'Average Confidence']
+    df = pd.DataFrame(data, columns=cols)
+    return df
 
+def show_imgs(df, time=1000):
+    imgs = df.iloc[3]
+    imgs = imgs.dropna()
+    for img in imgs:
+        cv2.imshow("IMG", img)
+        cv2.waitKey(time)
+    cv2.destroyAllWindows()
 
 def main():
     fname = "../uncal_test.pkl"
     df = pd.read_pickle(fname)
     summarize_stats(df)
+    show_imgs(df, time=2000)
 
 if __name__ == "__main__":
     main()
